b68438f457bb2edf9ae134f1459f1cb8725d6f5b
oinsup <oinsup@gmail.com>
2021-11-17 23:57:47 +0900

    create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './common.scss';
+import Header from './Header';
+import Toc from './Toc';
+import Map from './Map';
 import Table from './Table';
 const App = () => {
   return (
-    <div>
-      <Table tName="테이블제목"/>
-    </div>
+    <>
+      <Header/>
+      <div id="container">
+        <Toc/>
+        <Map/>
+      </div>
+    </>
   );
 };
 
