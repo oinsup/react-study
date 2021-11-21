a6158566cf0feb954daaa9aae5e7e78af2848d31
oinsup <oinsup@gmail.com>
2021-11-21 21:28:03 +0900

    pull

--- src/Toc.js
+++ src/Toc.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Toc = () => {
+  const useState[state,setState] = []
+  const onchange = () =>{
+    setState(["1"])
+    console.log(state)
+  }
   return (
     <div id="toc">
-      <input type="text"/>
+      <input type="text" onChange={onchange} />
       <button>버튼</button>
     </div>
   );
